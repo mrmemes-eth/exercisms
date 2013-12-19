a580ddee2512101eb1f86400e1fbdb67373840a7
Stephen Caudill <scaudill@gmail.com>
2013-12-18 21:36:30 -0700

    Don't step on a clojure.core method :\

--- nucleotide-count/nucleotide_count_test.clj
+++ nucleotide-count/nucleotide_count_test.clj
@@ -2,27 +2,27 @@
 (load-file "dna.clj")
 
 (deftest empty-dna-strand-has-no-adenosine
-  (is (= 0 (dna/count \A, ""))))
+  (is (= 0 (dna/nucleotide-count \A, ""))))
 
 (deftest empty-dna-strand-has-no-nucleotides
   (is (= {\A 0, \T 0, \C 0, \G 0}
          (dna/nucleotide-counts ""))))
 
 (deftest repetitive-cytidine-gets-counted
-  (is (= 5 (dna/count \C "CCCCC"))))
+  (is (= 5 (dna/nucleotide-count \C "CCCCC"))))
 
 (deftest repetitive-sequence-has-only-guanosine
   (is (= {\A 0, \T 0, \C 0, \G 8}
          (dna/nucleotide-counts "GGGGGGGG"))))
 
 (deftest counts-only-thymidine
-  (is (= 1 (dna/count \T "GGGGGTAACCCGG"))))
+  (is (= 1 (dna/nucleotide-count \T "GGGGGTAACCCGG"))))
 
 (deftest dna-has-no-uracil
-  (is (= 0 (dna/count \U "GATTACA"))))
+  (is (= 0 (dna/nucleotide-count \U "GATTACA"))))
 
 (deftest validates-nucleotides
-  (is (thrown-with-msg? Exception #"invalid nucleotide" (dna/count \X "GACT"))))
+  (is (thrown-with-msg? Exception #"invalid nucleotide" (dna/nucleotide-count \X "GACT"))))
 
 (deftest counts-all-nucleotides
   (let [s "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"]
